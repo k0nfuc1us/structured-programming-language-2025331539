af9fe01864109c3369c07e61b1cc32089cb73579
k0nfuc1us <srmobin2026@gmail.com>
2026-09-03 21:51:43 +0600

    Add files via upload

---
+++ array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // declaring an int array named numbers
+    int numbers[5];
+    
+    // inserting values for first & last element
+    
+    numbers[0]=10;
+    numbers[4]=50;
+    // printing values
+    printf("%d \n", numbers[0]);
+    printf("%d \n ", numbers[4]);
+    
+    return 0;
+
+}
